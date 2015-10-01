7609916fbcee5e3ed6a0bb92924b245554b551da
Nicola Guidi <nicola.guidi@me.com>
2015-10-01 15:03:32 +0000

    Secure app and more refactoring

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -15,12 +15,22 @@ class Instructor::CoursesController < ApplicationController
   end
 
   def show
-    @course = Course.find(params[:id])
   end
 
   private
 
-  def course_params
-    params.require(:course).permit(:title, :description, :cost)
-  end
+    def require_authorized_for_current_course
+      if current_course.user != current_user
+        render :text => "Unauthorized", :status => :unauthorized
+      end
+    end
+
+    helper_method :current_course
+    def current_course
+      @current_course ||= Course.find(params[:id])
+    end
+
+    def course_params
+      params.require(:course).permit(:title, :description, :cost)
+    end
 end
